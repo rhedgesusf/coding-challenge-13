3144676a2ab4b39ffdddb27e742cc7d4134b5611
Riley Hedges <rhedges1@usf.edu>
2025-03-04 18:48:32 -0500

    Task 1 - added comment

---
+++ cc_13.js
@@ -0,0 +1,8 @@
+/////////////////////////////////////////
+// Task 1: Creating the Base Structure //
+/////////////////////////////////////////
+
+console.log("--------------------------------------");
+console.log("Task 1: Creating the Base Structure");
+
+// Created base structure in HTML
