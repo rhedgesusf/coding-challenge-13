712f341aa2360ecb580a60b94ad01f8cae7c3312
Riley Hedges <rhedges1@usf.edu>
2025-03-04 19:10:22 -0500

    Task 2 - Employee Cards Dynamic Addition

--- cc_13.js
+++ cc_13.js
@@ -6,3 +6,38 @@ console.log("--------------------------------------");
 console.log("Task 1: Creating the Base Structure");
 
 // Created base structure in HTML
+
+///////////////////////////////////////////////
+// Task 2: Adding Employee Cards Dynamically //
+///////////////////////////////////////////////
+
+console.log("--------------------------------------");
+console.log("Task 2: Adding Employee Cards Dynamically");
+
+function createEmployee(name, position) {
+    const employeeContainer = document.getElementById("employeeContainer");
+
+    if(employeeContainer) {
+        const card = document.createElement('div');
+
+        card.setAttribute('class', 'employee-card');
+        card.setAttribute('id', 'employeeCard');
+    
+        card.innerHTML = `<div><h3>${name}</h3>
+                           <p>${position}</p>
+        </div>`;
+        
+        const button = document.createElement('button');
+        button.textContent = 'Remove';
+        button.addEventListener('click', () => {
+            employeeContainer.removeChild(card);
+        });
+        card.appendChild(button);
+
+        employeeContainer.appendChild(card);    
+    }
+}
+
+createEmployee("John Doe", "Manager");
+createEmployee("Mike Miller", "Supervisor");
+createEmployee("Jerry Wilken", "Electrician");
